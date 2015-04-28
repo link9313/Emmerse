b1027b056c534375d621d71730da0035904aa5c8
Nick Lauber <link9313@gmail.com>
2015-04-28 15:54:04 -0500

    Multiple 360 Videos
    This adds two more 360 videos: an orchestra concert and a rock concert.

--- Assets/Scripts/Reticle.cs
+++ Assets/Scripts/Reticle.cs
@@ -1,31 +1,41 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Reticle : MonoBehaviour {
+public class Reticle : MonoBehaviour
+{
 	public Camera CameraFacing;
 	private Vector3 originalScale;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 		originalScale = transform.localScale;
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 		RaycastHit hit;
 		float distance;
+		
 		if (Physics.Raycast (new Ray (CameraFacing.transform.position,
-		                             CameraFacing.transform.rotation * Vector3.forward),
-		                     out hit)) {
+			CameraFacing.transform.rotation * Vector3.forward), out hit))
+		{
 			distance = hit.distance;
-		} else {
+		}
+		else
+		{
 			distance = CameraFacing.farClipPlane * 0.95f;
 		}
+
 		transform.position = CameraFacing.transform.position +
 			CameraFacing.transform.rotation * Vector3.forward * distance;
+		
 		transform.LookAt (CameraFacing.transform.position);
 		transform.Rotate (0.0f, 180.0f, 0.0f);
-		if (distance < 10.0f) {
+		
+		if (distance < 10.0f)
+		{
 			distance *= 1 + 5*Mathf.Exp (-distance);
 		}
 		transform.localScale = originalScale * distance;
